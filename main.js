9c9fb483ae45abf9f96f2c2ecb86fae95daa31fe
dakshc3109 <78908644+dakshc3109@users.noreply.github.com>
2021-07-23 20:18:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 //main.js
+
+Webcam.set({
+    width: 340,
+    height: 300,
+    image_format: "png",
+    image_quality: 90
+});
+
+var camera = document.getElementById("camera");
+
+Webcam.attach("#camera");
