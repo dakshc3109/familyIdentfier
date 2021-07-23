0e8a5f86196315c8ce78afbe36ad9838f6025241
dakshc3109 <78908644+dakshc3109@users.noreply.github.com>
2021-07-23 20:33:27 +0530

    adding webcam snap and view and ml5 version

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//main.js
+//This is main.js
 
 Webcam.set({
     width: 340,
@@ -10,3 +10,17 @@ Webcam.set({
 var camera = document.getElementById("camera");
 
 Webcam.attach("#camera");
+
+function capture(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = "<img id='captured_img' src='"+data_uri+"'>";
+    });
+}
+
+console.log("ml5 version: ",ml5.version);
+
+var classifier = ml5.imageClassifier("https://storage.googleapis.com/tm-model/-57PUIvXf/model.json", modelLoaded);
+
+function modelLoaded(){
+    console.log("model loaded!");
+};
